7f01bae573e4dde77808a457886b0ef8a0ae3c21
GodProZEE <100044640+GodProZEE@users.noreply.github.com>
2024-11-26 17:32:03 +0500

    Add function to put a value on the grid
    Takes the value based on the user, then adds that value onto the grid.

--- lib/board.rb
+++ lib/board.rb
@@ -3,9 +3,20 @@ class Board
   def initialize
     @grid = [[0, 1, 2], [3, 4, 5], [6, 7, 8]] 
   end
+
+  def add_value(player)
+    puts "Please enter a number between 0 and 8 to place your choice there: "
+    # @entry = gets.chomp.to_i
+    @entry = 7
+    for i in @grid
+      for j in i
+        if j == @entry
+          @grid[@grid.find_index(i)][i.find_index(j)] = player.player_choice
+        end
+      end
+    end
+    
+  end
 end
 
-hi = Board.new
-hi.grid.each do |elem|
-  p elem
-end
+
